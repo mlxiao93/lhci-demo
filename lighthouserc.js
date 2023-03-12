9db1445ce82181fef59a8af1ae0f596d2bde3895
maolinxiao <maolinxiao@tencent.com>
2023-03-12 20:49:31 +0800

    add lang

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,7 +4,9 @@ module.exports = {
       staticDistDir: './public',
     },
     upload: {
-      target: 'temporary-public-storage',
+      target: 'lhci',
+      serverBase: 'http://localhost:9001',
+      token: 'ddf6f610-6014-405b-a184-f0852767c606',
     },
   },
 };
