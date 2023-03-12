1233bc85c3de12ed028899ba8980824bd7f81121
maolinxiao <maolinxiao@tencent.com>
2023-03-12 21:55:45 +0800

    update

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,10 +3,13 @@ module.exports = {
     collect: {
       staticDistDir: './public',
     },
-    upload: {
-      target: 'lhci',
-      serverBase: 'http://localhost:9001',
-      token: 'ddf6f610-6014-405b-a184-f0852767c606',
-    },
+    // upload: {
+    //   target: 'lhci',
+    //   serverBase: 'http://localhost:9001',
+    //   token: 'ddf6f610-6014-405b-a184-f0852767c606',
+    // },
+    asserts: {
+      preset: 'lighthouse:recommended',
+    }
   },
 };
